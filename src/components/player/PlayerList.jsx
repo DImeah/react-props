77c6c048f486da5116024a4f63f2174d4cc774a4
Imeah <imeahdornu@gmail.com>
2023-11-20 09:23:41 +0100

    Added comments

--- src/components/player/PlayerList.jsx
+++ src/components/player/PlayerList.jsx
@@ -1,10 +1,13 @@
 // Players List
+// Importing the Player component and playersData from external file
 import Player from "./Player";
 import { playersData } from "../../players";
 
+// PlayersList component definition
 const PlayersList = () => {
   return (
     <div
+      // Container div with background styling for the players list
       className="has-bg-img bg-secondary bg-blend-screen bg-img bg-cover"
       style={{
         display: "flex",
@@ -16,11 +19,13 @@ const PlayersList = () => {
         boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
       }}
     >
+      {/* Mapping through the playersData array and rendering Player components */}
       {playersData.map((player, id) => (
-        <Player key={id} {...player} />
+        <Player key={id} {...player} /> // Used a spread operator to pass player details as props
       ))}
     </div>
   );
 };
 
+// Exporting the PlayersList component as the default export
 export default PlayersList;
